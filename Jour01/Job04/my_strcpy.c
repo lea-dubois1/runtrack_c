bd4b4842029ea7818fabac96b8fe97163a462064
lea-dubois1 <lea.dubois@laplateforme.io>
2023-11-27 16:04:28 +0100

    jour 1 job 4 corrigé

--- Jour01/Job04/my_strcpy.c
+++ Jour01/Job04/my_strcpy.c
@@ -1,21 +1,8 @@
 #include <unistd.h>
 #include <malloc.h>
 
-char *my_strcpy(char *dest, const char *src){
-    int length = 0;
-
-    while(src[length] != 0) {
-        length++;
-    }
-
-    dest = (char*)malloc(sizeof(char) * length);
-
-    length = 0;
-
-    while(src[length] != 0){
-        dest[length] = src[length];
-        length++;
-    }
+char *my_strcpy(char *dest, char *src){
+    dest = src;
 
     return dest;
 }
