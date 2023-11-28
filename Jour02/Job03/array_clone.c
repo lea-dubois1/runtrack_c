6b685092448d6db588b62ae212b546402e6ac5f5
lea-dubois1 <lea.dubois@laplateforme.io>
2023-11-28 14:40:46 +0100

    jour 2 job 3 corrigé

--- Jour02/Job03/array_clone.c
+++ Jour02/Job03/array_clone.c
@@ -14,34 +14,3 @@ char **array_clone(char **strs, int n){
 
     return result;
 }
-
-int main(){
-    char **strings = malloc(sizeof(char*) * 9);
-    int i = 0;
-
-    while(i < 9){
-        strings[i] = malloc(sizeof(char) * 9);
-        i++;
-    }
-
-    strings[0] = "number 1";
-    strings[1] = "number 2";
-    strings[2] = "number 3";
-    strings[3] = "number 4";
-    strings[4] = "number 5";
-    strings[5] = "number 6";
-    strings[6] = "number 7";
-    strings[7] = "number 8";
-    strings[8] = 0;
-
-    char **result = array_clone(strings, 5);
-
-    int j = 0;
-
-    while(result[j] != 0){
-        printf("%s\n", result[j]);
-        j++;
-    }
-
-    return 0;
-}
