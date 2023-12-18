397e794dedf5bdd191dbd358bd7c220c5b9275bc
lea-dubois1 <lea.dubois@laplateforme.io>
2023-12-18 11:21:06 +0100

    Jour 2 job 6 terminé

--- Jour02/Job06/delete_str_array.c
+++ Jour02/Job06/delete_str_array.c
@@ -1,3 +1,11 @@
-//
-// Created by lea on 18/12/23.
-//
+#include <malloc.h>
+
+void delete_str_array(char ***strs){
+    for (int i = 0; strs[i] != 0; ++i) {
+        for (int j = 0; strs[i][j] != 0; ++j) {
+            free(strs[i][j]);
+        }
+        free(strs[i]);
+    }
+    strs[0] = NULL;
+}
